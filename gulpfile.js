84876b9a487a26f95b1a7cdca2802b33fc63690f
puliwang@cxist.com <puliwang@cxist.com>
2022-02-25 16:36:36 +0800

    添加部署配置

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,75 @@ const { src, dest, series } = require("gulp");
 const sass = require("gulp-dart-sass");
 const fs = require('fs');
 const path = require('path');
+const del = require("del");
 const rename = require('gulp-rename');
 const concat = require('gulp-concat');
 
+const platformManagementDist = `C://Leon/Codes/CXIST/Mirror.PlatformManagement.Frontend/dist`;
+const businessManagementDist = `C://Leon/Codes/CXIST/Mirror.BusinessManagement.Frontend/dist`;
+const workstationDomain = 'http://cxist-dev.com';
+const envConfigFileName = 'env-config.json';
 
-// function compileStationSass(cb) {
-//     src("projects/workstation-shared/resources/workstation-shared.scss").pipe(sass())
-//         .on('error', cb)
-//         .pipe(rename('workstation-shared.css'))
-//         .pipe(dest("dist/workstation-shared/resources")).on('end', cb);
-// }
+const platformManagementEntry = `${workstationDomain}/platform-management`;
+const businessManagementEntry = `${workstationDomain}/business-management`;
+const businessConfigurationEntry = `${workstationDomain}/business-configuration`;
+const businessForegroundEntry = `${workstationDomain}/business-foreground`;
 
-// function copyZorrorTheme(cb) {
-//     src("projects/workstation-shared/resources/zorror-theme.less")
-//         .on('error', cb)
-//         .pipe(dest("dist/workstation-shared/resources")).on('end', cb);
-// }
+function readEnvConfig(configPath) {
+    const str = fs.readFileSync(configPath, { encoding: 'utf8' });
+    if (!str) { return {}; }
+    return JSON.parse(str);
+}
 
-// exports.compileCommonResources = series(compileStationSass, copyZorrorTheme);
+function updateEnvConfig(configPath, config) {
+    fs.writeFileSync(configPath, JSON.stringify(config), { encoding: 'utf8' });
+}
+
+function updateAppEnv(cb) {
+    const businessManagementConfigPath = `./business-management/dist/assets/${envConfigFileName}`;
+    let envConfig;
+    if (fs.existsSync(businessManagementConfigPath)) {
+        envConfig = readEnvConfig(businessManagementConfigPath);
+        envConfig.platformManagementEntry = platformManagementEntry;
+        envConfig.businessConfigurationEntry = businessConfigurationEntry;
+        envConfig.businessForegroundEntry = businessForegroundEntry;
+        updateEnvConfig(businessManagementConfigPath, envConfig);
+    }
+
+    const platformManagementConfigPath = `./platform-management/dist/assets/${envConfigFileName}`;
+    if (fs.existsSync(platformManagementConfigPath)) {
+        envConfig = readEnvConfig(platformManagementConfigPath);
+        envConfig.businessConfigurationEntry = businessConfigurationEntry;
+        envConfig.businessForegroundEntry = businessForegroundEntry;
+        envConfig.businessManagementEntry = businessManagementEntry;
+        updateEnvConfig(platformManagementConfigPath, envConfig);
+    }
+
+
+    cb();
+}
+
+function copyPlatformManagementDist(cb) {
+    del.sync([`platform-management/dist/**`]);
+    src(`${platformManagementDist}/mirror-platform-management/**`)
+        .pipe(dest(`platform-management/dist`))
+        .on('end', () => {
+            console.log('copy finished');
+            cb();
+        });
+}
+
+function copyBusinessManagementDist(cb) {
+    del.sync([`business-management/dist/**`]);
+    src(`${businessManagementDist}/mirror-business-management/**`)
+        .pipe(dest(`business-management/dist`))
+        .on('end', () => {
+            console.log('copy finished');
+            cb();
+        });
+}
+
+exports.copyPlatformManagement = series(copyPlatformManagementDist);
+exports.copyBusinessManagement = series(copyBusinessManagementDist);
+exports.updateAppEnv = series(updateAppEnv);
+
